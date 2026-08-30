ac73714f7056f6331e6b617125558704b0e15949
JuanSalvadorParra <juan.parra.rincon@est.una.ac.cr>
2026-08-30 08:08:00 -0600

    Comentarios en los archivos de los datos

--- src/main/java/una/sistemareservas/datos/CategoriaRecursoDatos.java
+++ src/main/java/una/sistemareservas/datos/CategoriaRecursoDatos.java
@@ -10,37 +10,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CategoriaRecursoDatos {
+    // Archivo de las categorias de recursos
     private static final Path archivoCategoriasR = Path.of("src/main/resources/datos/categorias.json");
 
+    // Carga los datos de un archivo
     public List<CategoriaRecursoDTO> serializar() throws IOException {
+        // Crea una lista para las categorias sacadas del archivo
         List<CategoriaRecursoDTO> categorias = new ArrayList<>();
 
+        // Si el archivo no existe devuelvo la lista vacia
         if (!Files.exists(archivoCategoriasR)) {
             return categorias;
         }
 
+        // Leo el archivo
         String contenido = Files.readString(archivoCategoriasR);
 
+        // Array JSON para manejar el contenido
         JSONArray listaCategoriasR = new JSONArray(contenido);
 
+        // For que itera en el array JSON, y obtiene los datos para construir un DTO de categoria recurso y añadirlo a la lista
         for (int i = 0; i < listaCategoriasR.length(); i++) {
             categorias.add(new CategoriaRecursoDTO(listaCategoriasR.getJSONObject(i).getString("id"), listaCategoriasR.getJSONObject(i).getString("descripcion")));
         }
 
+        // Devuelve la lista
         return categorias;
     }
 
+    // Funcion para cargar datos a un archivo
+    // Recibe una lista de CategoriasRecursoDTO
     public void deserializar(List<CategoriaRecursoDTO> categoriasR) throws IOException {
+        // Array en JSON para guardar en el archivo
         JSONArray listaCategoriasR = new JSONArray();
 
+        // Itero en la lista de las categorias
         for (CategoriaRecursoDTO categoriaR : categoriasR) {
+            // Creo un nuevo objeto JSON, y le añado los datos necesarios
             JSONObject objeto = new JSONObject();
             objeto.put("id", categoriaR.getID());
             objeto.put("descripcion", categoriaR.getDescripcion());
+            // Inserto el "objeto" completo en la lista, eso para cada categoriaRecursoDTO de la lista
             listaCategoriasR.put(objeto);
         }
 
+        // Por si no existen el archivo y/o las carpetas, los crea
         Files.createDirectories(archivoCategoriasR.getParent());
+
+        // Sobrescribe el archivo con los datos de la lista pasada por parametro
         Files.writeString(archivoCategoriasR, listaCategoriasR.toString(2));
     }
 }
--- src/main/java/una/sistemareservas/datos/RecursoDatos.java
+++ src/main/java/una/sistemareservas/datos/RecursoDatos.java
@@ -11,28 +11,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RecursoDatos {
+    // Archivo de los recursos
     private static final Path archivoRecursos = Path.of("src/main/resources/datos/recursos.json");
 
+    // Carga los datos de un archivo
+    // Recibo la lista con todas las categorias existentes de parametro
     public List<RecursoDTO> serializar(List<CategoriaRecursoDTO> categorias) throws IOException {
+        // Crea una lista para los recursos sacados del archivo
         List<RecursoDTO> recursos = new ArrayList<>();
 
+        // Si el archivo no existe devuelvo la lista vacia
         if (!Files.exists(archivoRecursos)) {
             return recursos;
         }
 
+        // Leo el archivo
         String contenido = Files.readString(archivoRecursos);
 
+        // Array JSON para manejar el contenido
         JSONArray listaRecursos = new JSONArray(contenido);
 
+        // For que itera en el array JSON
         for (int i = 0; i < listaRecursos.length(); i++) {
+            // Obtengo un "objeto" json de la lista
             JSONObject objeto = new JSONObject(listaRecursos.getJSONObject(i));
 
+            // Saco los datos de cada "objeto recurso"
             String id = objeto.getString("id");
             String descripcion = objeto.getString("descripcion");
             String categoriaId = objeto.getString("categoriaId");
 
+            // Reviso si el ID de la categoria esta en la lista de todas las categorias
             CategoriaRecursoDTO categoria = null;
-
             for (CategoriaRecursoDTO c : categorias) {
                 if (c.getID() == categoriaId) {
                     categoria = c;
@@ -40,24 +50,35 @@ public class RecursoDatos {
                 }
             }
 
+            // Construyo el RecursoDTO y lo añado a la lista final
             recursos.add(new RecursoDTO(id, descripcion, categoria));
         }
 
+        // Devuelvo la lista de recursos
         return recursos;
     }
 
+    // Funcion para cargar datos a un archivo
+    // Recibe una lista de RecursoDTO
     public void deserializar(List<RecursoDTO> recursos) throws IOException {
+        // Array en JSON para guardar en el archivo
         JSONArray listaRecursos = new JSONArray();
 
+        // Itero en la lista de recursos
         for (RecursoDTO recurso : recursos) {
+            // Creo un nuevo objeto JSON, y le añado los datos necesarios
             JSONObject item = new JSONObject();
             item.put("id", recurso.getID());
             item.put("descripcion", recurso.getDescripcion());
             item.put("IDcategoria", recurso.getCategoria().getID());
+            // Inserto el "objeto" completo en la lista final, eso para cada RecursoDTO de la lista a guardar
             listaRecursos.put(item);
         }
 
+        // Por si no existen el archivo y/o las carpetas, los crea
         Files.createDirectories(archivoRecursos.getParent());
+
+        // Sobrescribe el archivo con los datos de la lista pasada por parametro
         Files.writeString(archivoRecursos, listaRecursos.toString(2));
     }
 }
